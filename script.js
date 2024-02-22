6a9cf0b23aff419215e0ea061792cf381f0455b6
Kieran Wake <kieranwake1234@gmail.com>
2024-02-22 22:59:39 +0000

    use the equality operator to reassign a value. Don't make the mistake of concatenating the string

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//
+//Challenge 1: Testing Assignment Operators! 
+//Description: Declare some variables and test the difference between - 
+//standard equality operator and various assignment operators. 
+
+function testAssignment() {
+    let sum = "30"; 
+    sum = Number(sum) + 40; 
+    console.log(Number(sum)); 
+}
+testAssignment();
