a20aac7a5cad53b863d12abaac875a2101da4b6b
Kieran Wake <kieranwake1234@gmail.com>
2024-02-24 18:13:45 +0000

    declared challenge requirements

--- script.js
+++ script.js
@@ -59,3 +59,22 @@ function operatorTest() {
     console.log(x += y);
 }
 operatorTest();
+
+//Challenge 4: Sizing a Canvas Box! 
+//Requirements: Change the line that calculates x so the box is still 50px wide, but the 50 -
+//is calculated using the numbers 43 and 7 and an arithmetic operator.
+
+//Change the line that calculates y so the box is 75px high, but the 75 is calculated using the -
+//numbers 25 and 3 and an arithmetic operator.
+
+//Change the line that calculates x so the box is 250px wide, but the 250 is calculated using -
+//two numbers and the remainder (modulo) operator.
+
+//Change the line that calculates y so the box is 150px high, but the 150 is calculated using -
+//three numbers and the subtraction and division operators.
+
+//Change the line that calculates x so the box is 200px wide, but the 200 is calculated using -
+//the number 4 and an assignment operator.
+
+//Change the line that calculates y so the box is 200px high, but the 200 is calculated using -
+//the numbers 50 and 3, the multiplication operator, and the addition assignment operator.
